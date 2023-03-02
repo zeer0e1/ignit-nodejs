efa5da234d28033287886a0a1e1f6bedfc7bd815
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-03-01 22:07:11 -0300

    Update index.js
    Criando as rotas da aplicação

--- aulas/01-fudamentos-node/src/index.js
+++ aulas/01-fudamentos-node/src/index.js
@@ -2,8 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, resp) => {
-  return resp.json({ message: "Hello World Ignite Node js!" });
+app.use(express.json());
+
+app.get("/courses", (req, resp) => {
+  const query = req.query;
+  console.log(query);
+  return resp.json(["Curso1", "Curso2", "Curso3"]);
+});
+
+app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 2", "Curso 4"]);
 });
 
 app.listen(3333);
