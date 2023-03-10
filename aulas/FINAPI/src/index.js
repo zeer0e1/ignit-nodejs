7b9ada5976ea479fb66670272cbbb9200d0939cc
Zeer0e1 <lucasfreitasr@outlook.com>
2023-03-10 09:13:56 -0300

    Finalizando rotas da API

--- aulas/FINAPI/src/index.js
+++ aulas/FINAPI/src/index.js
@@ -4,6 +4,31 @@ const costumers = [];
 
 const app = express();
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statemet) {
+  const balance = statemet.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.use(express.json());
 
 app.post("/account", (req, res) => {
@@ -24,6 +49,47 @@ app.post("/account", (req, res) => {
   });
 
   return res.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request.customer;
+  const { date } = request.query;
+
+  const dateFormat = new Date(data + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateStrng() === new Date(dateFormat).toDateStrng()
+  );
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { costumer } = request;
+  costumer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  costumers.splice(costumers.indexOf(customer), 1);
+
+  return response.status(200).json(costumers).send();
+});
+
+app.get("/balance", verifyIfExistsBalance, (request, response) => {
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+  return response.json({ balance });
 });
 
 app.listen(3333);
